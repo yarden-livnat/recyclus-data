6b996b2015b89265bb0362f508c6f0cf0615e735
Yarden Livnat <yarden.livnat@gmail.com>
2019-03-05 18:06:59 -0700

    cleanup

--- recyclus_data/__init__.py
+++ recyclus_data/__init__.py
@@ -28,27 +28,4 @@ def create_app(config_name='development'):
 
     print(f'**** recyclus_datastore [{config_name}] created')
 
-    test(app)
     return app
-
-from pymongo import MongoClient
-from flask_pymongo import PyMongo
-
-def test(app):
-    # print('test mongo')
-    # client = MongoClient('mongodb://datastore-db:27017')
-    # db = client.files
-    # db.test.insert_one({'name': 'test'})
-    # print('check test:', db.collection_names(include_system_collections=False))
-    #
-    # print('test flask_mongo')
-    #
-    # m = PyMongo(app)
-    # print('check flask test:', m.db.collection_names(include_system_collections=False))
-
-    print('original')
-    print('datastore', mongo)
-    print('db', mongo.db)
-    print('check flask test:', mongo.db.collection_names(include_system_collections=False))
-    print('app.config', app.config['MONGO_URI'])
-
